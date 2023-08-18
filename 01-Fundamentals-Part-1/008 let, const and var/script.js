104de6c0658e389866de33095e93f3afd33aa966
Sourav Das <souravdas4816@gmail.com>
2023-08-18 20:37:51 +0530

    let, const and var

---
+++ 01-Fundamentals-Part-1/008 let, const and var/script.js
@@ -0,0 +1,60 @@
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+// birthYear = 1999;
+
+// const job;
+
+var job = 'coder';
+job = 'learner';
+
+lastName = "Das";
+lastName = 'pond';
+console.log(lastName);
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Sourav");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Sourav");
+console.log(23);
+
+let lastName = "Das";
+
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let sourav_das = "SD";
+let $function = 28;
+
+let person = "Sourav";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Learner";
+
+console.log(myFirstJob);
+*/
