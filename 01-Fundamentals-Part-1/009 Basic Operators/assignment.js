d31bcb10f8aba9b83526bf7137a4906636d25536
Sourav Das <souravdas4816@gmail.com>
2023-08-18 22:10:36 +0530

    Basic Operators

---
+++ 01-Fundamentals-Part-1/009 Basic Operators/assignment.js
@@ -0,0 +1,12 @@
+const country = 'India';
+const continent = "Asia";
+let population = 130;
+const isIsland = false;
+let language = "hindi";
+
+isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
