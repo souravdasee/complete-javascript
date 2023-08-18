f667b826d4f5dae35ad847a9643e900390362f05
Sourav Das <souravdas4816@gmail.com>
2023-08-18 19:51:22 +0530

    Data Types

---
+++ 01-Fundamentals-Part-1/007 Data Types/script.js
@@ -0,0 +1,44 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Sourav");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Sourav");
+console.log(23);
+
+let lastName = "Das";
+
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let sourav_das = "SD";
+let $function = 28;
+
+let person = "Sourav";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Learner";
+
+console.log(myFirstJob);
+*/
