17d658ffce1eca87679159da12c3280bf6175d6c
Sourav Das <souravdas4816@gmail.com>
2023-08-18 16:40:40 +0530

    005 Values and Variables

---
+++ 01-Fundamentals-Part-1/005 Values and Variables/script.js
@@ -0,0 +1,22 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Sourav");
+console.log(23);
+
+let lastName = "Das";
+
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let sourav_das = "SD";
+let $function = 28;
+
+let person = "Sourav";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Learner";
+
+console.log(myFirstJob);
