512d1ccfaf32722dca41cab1f20201e89ce804c5
Sourav Das <souravdas4816@gmail.com>
2023-08-20 19:35:41 +0530

    Taking Decisions if  else Statements

---
+++ 01-Fundamentals-Part-1/014 Taking Decisions if  else Statements/assignment.js
@@ -0,0 +1,21 @@
+const country = 'India';
+const continent = "Asia";
+let population = 130;
+const isIsland = false;
+let language = "hindi";
+
+population /= 2;
+population++;
+
+const popFinland = 6;
+const popAvgCountry = 33;
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+
+const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+// console.log(population);
+// console.log(population > popFinland);
+// console.log(population < popAvgCountry);
+// console.log(description);
+console.log(description1);
---
+++ 01-Fundamentals-Part-1/014 Taking Decisions if  else Statements/script.js
@@ -0,0 +1,149 @@
+const age = 15;
+
+if (age >= 18) {
+    console.log('Bob can start driving 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Bob is too young. Wait another ${yearsLeft} years 😊`);
+}
+
+const birthYear = 1997;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+
+
+/*
+const firstName = 'Sourav';
+const job = 'learner';
+const birthYear = 1997;
+const year = 2038;
+
+const sourav = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(sourav);
+
+const souravNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(souravNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+
+
+const now = 2037;
+const ageSourav = now - 1997;
+const ageBob = now - 2001;
+
+console.log(now - 1997 > now - 2001);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageBob + ageSourav) / 2;
+console.log(ageBob, ageSourav, averageAge);
+
+
+// Math Operators
+const now = 2037;
+const ageSourav = now - 1997;
+const ageBob = now - 2001;
+console.log(ageSourav, ageBob);
+
+console.log(ageSourav * 2, ageSourav/10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Sourav";
+const lastName = "Das";
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+
+console.log(x);
+
+// Comparison operators
+console.log(ageSourav > ageBob); // >, <, >=, <=
+console.log(ageBob >= 18);
+
+const isFullAge = ageBob >= 18;
+
+console.log(now - 1997 > now - 2001);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+// birthYear = 1999;
+
+// const job;
+
+var job = 'coder';
+job = 'learner';
+
+lastName = "Das";
+lastName = 'pond';
+console.log(lastName);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Sourav");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Sourav");
+console.log(23);
+
+let lastName = "Das";
+
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let sourav_das = "SD";
+let $function = 28;
+
+let person = "Sourav";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Learner";
+
+console.log(myFirstJob);
+*/
