21492d773bf9b607caaee6b81d8f676341e552ce
Sourav Das <souravdas4816@gmail.com>
2023-08-20 19:49:26 +0530

    Taking Decisions: if / else Statements Assignments

--- 01-Fundamentals-Part-1/014 Taking Decisions if  else Statements/assignment.js
+++ 01-Fundamentals-Part-1/014 Taking Decisions if  else Statements/assignment.js
@@ -3,19 +3,23 @@ const continent = "Asia";
 let population = 130;
 const isIsland = false;
 let language = "hindi";
-
-population /= 2;
-population++;
-
 const popFinland = 6;
 const popAvgCountry = 33;
 
-const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+// population /= 2;
+// population++;
 
+if (population > popAvgCountry) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population is ${popAvgCountry - population} million below average`);
+}
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
 const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
 // console.log(population);
 // console.log(population > popFinland);
 // console.log(population < popAvgCountry);
 // console.log(description);
-console.log(description1);
+// console.log(description1);
