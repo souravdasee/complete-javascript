8726233fc8759cd60f8710a5150338e989ab8514
Sourav Das <souravdas4816@gmail.com>
2023-08-20 17:57:20 +0530

    Strings and Template Literals Assignment

--- 01-Fundamentals-Part-1/013 Strings and Template Literals/assignment.js
+++ 01-Fundamentals-Part-1/013 Strings and Template Literals/assignment.js
@@ -8,15 +8,14 @@ population /= 2;
 population++;
 
 const popFinland = 6;
-
 const popAvgCountry = 33;
 
 const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
 
-console.log(population);
+const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
-console.log(population > popFinland);
-
-console.log(population < popAvgCountry);
-
-console.log(description);
+// console.log(population);
+// console.log(population > popFinland);
+// console.log(population < popAvgCountry);
+// console.log(description);
+console.log(description1);
