2d5c952c4f160c2e2feb283878dcb3af686a8fb3
Sourav Das <souravdas4816@gmail.com>
2023-08-22 12:29:03 +0530

    Coding Challenge #3

--- 01-Fundamentals-Part-1/019 Boolean Logic/codingchallenge.js
+++ 01-Fundamentals-Part-1/019 Boolean Logic/codingchallenge.js
@@ -1,18 +1,34 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn ** 2); 
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn ** 2); 
 
-// console.log (BMIMark, BMIJohn);
+// // console.log (BMIMark, BMIJohn);
 
-const markHigherBMI = BMIMark > BMIJohn;
-// console.log(markHigherBMI);
+// const markHigherBMI = BMIMark > BMIJohn;
+// // console.log(markHigherBMI);
 
-if (markHigherBMI) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// // if (markHigherBMI) {
+// //     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// // } else {
+// //     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// // }
+
+const avgDolphins = (97 + 112 + 101) / 3;
+const avgKoalas = (109 + 95 + 123) / 3;
+const minScore = 100;
+
+console.log(avgDolphins, avgKoalas);
+
+if ((avgDolphins > avgKoalas) && (avgDolphins >= minScore)) {
+    console.log('Winner is Dolphins');
+} else if ((avgDolphins === avgKoalas) && (avgDolphins>= minScore) && (avgKoalas >= minScore)) {
+    console.log('Draw');
+} else if ((avgDolphins < avgKoalas) && (avgKoalas >= minScore)) {
+    console.log('Winner is Koalas');
 } else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+    console.log('Winner have not minimun score');
 }
