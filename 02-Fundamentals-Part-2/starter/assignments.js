2ec8283099d6cdd1f7e2a66f7b6230e91d77fa4a
Sourav Das <souravdas4816@gmail.com>
2023-08-22 20:37:06 +0530

    Arrow Functions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -9,16 +9,16 @@
 // const countryFinland = console.log(describeCountry('Finland', 6, 'Helsinki'));
 // const countryUSA = console.log(describeCountry('USA', 100, 'Washington D.C.'));
 
-function percentageOfWorld1(population) {
-    return (population / 7900) * 100;
-}
-console.log(`China has 1441 million people, so it's about ${percentageOfWorld1(1441)}% of the world population`);
-console.log(`India has 1700 million people, so it's about ${percentageOfWorld1(1700)}% of the world population`);
-console.log(`USA has 1100 million people, so it's about ${percentageOfWorld1(1100)}% of the world population`);
+// function percentageOfWorld1(population) {
+//     return (population / 7900) * 100;
+// }
+// console.log(`China has 1441 million people, so it's about ${percentageOfWorld1(1441)}% of the world population`);
+// console.log(`India has 1700 million people, so it's about ${percentageOfWorld1(1700)}% of the world population`);
+// console.log(`USA has 1100 million people, so it's about ${percentageOfWorld1(1100)}% of the world population`);
 
-const percentageOfWorld2 = function(population) {
-    return (population / 7900) * 100;
-}
-console.log(`China has 1441 million people, so it's about ${percentageOfWorld1(1441)}% of the world population`);
-console.log(`India has 1700 million people, so it's about ${percentageOfWorld1(1700)}% of the world population`);
-console.log(`USA has 332 million people, so it's about ${percentageOfWorld1(332)}% of the world population`);
+// const percentageOfWorld2 = function(population) {
+//     return (population / 7900) * 100;
+// }
+// console.log(`China has 1441 million people, so it's about ${percentageOfWorld1(1441)}% of the world population`);
+// console.log(`India has 1700 million people, so it's about ${percentageOfWorld1(1700)}% of the world population`);
+// console.log(`USA has 332 million people, so it's about ${percentageOfWorld1(332)}% of the world population`);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1997);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 60 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1997, 'Sourav'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
 /*
 ////////////////////////////////////////////////////////////////////
 // Function declaration
