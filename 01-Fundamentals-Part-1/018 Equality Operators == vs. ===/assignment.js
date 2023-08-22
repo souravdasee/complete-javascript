38a286d455c6e448e2f4d558fffd067d114ea31e
Sourav Das <souravdas4816@gmail.com>
2023-08-22 10:07:19 +0530

    Equality Operators: == vs. === Assignment

--- 01-Fundamentals-Part-1/018 Equality Operators == vs. ===/assignment.js
+++ 01-Fundamentals-Part-1/018 Equality Operators == vs. ===/assignment.js
@@ -5,6 +5,7 @@ const isIsland = false;
 let language = "hindi";
 const popFinland = 6;
 const popAvgCountry = 33;
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
 
 // population /= 2;
 // population++;
@@ -15,6 +16,14 @@ if (population > popAvgCountry) {
     // console.log(`${country}'s population is ${popAvgCountry - population} million below average`);
 }
 
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+
 const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
 const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
@@ -23,8 +32,8 @@ const description1 = `${country} is in ${continent}, and its ${population} milli
 // console.log(population < popAvgCountry);
 // console.log(description);
 // console.log(description1);
-console.log('9' - '5');
-console.log('19' - '13' + '17');
-console.log('19' - '13' + 17);
-console.log('123' < 57);
-console.log(5 + 6 + '4' + 9 - 4 - 2);
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
