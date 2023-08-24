57d2160e606acd5fa62283e900fee71fda7fbcf1
Sourav Das <souravdas4816@gmail.com>
2023-08-24 16:10:36 +0530

    Introduction to Arrays Assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,8 +1,20 @@
 'use strict';
 
-// function percentageOfWorld1(population) {
-//     return (population / 7900) * 100;
-// }
+const populations = [1441, 1700, 332, 60];
+
+console.log(populations.length === 4);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentages = [
+    percentageOfWorld1(populations[0]), 
+    percentageOfWorld1(populations[1]), 
+    percentageOfWorld1(populations[2]), 
+    percentageOfWorld1(populations[3])
+];
+console.log(percentages);
 
 // const describePopulation = (country, population) => `${country} has ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`
 
