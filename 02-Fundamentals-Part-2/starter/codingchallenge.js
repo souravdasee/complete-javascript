517f56e772b71783a9792b588a21bcd7ee99e609
Sourav Das <souravdas4816@gmail.com>
2023-08-24 19:25:14 +0530

    Coding Challenge 2

--- 02-Fundamentals-Part-2/starter/codingchallenge.js
+++ 02-Fundamentals-Part-2/starter/codingchallenge.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const bills = [125, 555, 44];
+const calcTip = bills => bills >= 50 && bills<= 300 ? bills * 0.15 : bills * 0.2;
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [
+    bills[0] + tips[0], 
+    bills[1] + tips[1], 
+    bills[2] + tips[2]
+];
+
+console.log(bills, tips, total);
+
+/*
+//////////////////////////////////////////////////////////////////////////
 const calcAverage = (s1, s2, s3) => (s1 + s2 + s3) / 3;
 let scoreDolphins = calcAverage(85, 54, 41);
 let scoreKoalas = calcAverage(23, 34, 27);
@@ -14,3 +27,4 @@ const checkWinner = function(avgDolphins, avgKoalas) {
     }
 }
 checkWinner(scoreDolphins, scoreKoalas);
+*/
