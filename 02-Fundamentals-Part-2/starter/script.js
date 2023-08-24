751f8c9fe176bb5dba1919e4da605c95c91bf16a
Sourav Das <souravdas4816@gmail.com>
2023-08-24 16:00:43 +0530

    Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const friend1 = 'Franklin';
+const friend2 = 'Michel';
+const friend3 = 'Trevor';
+
+const friends = ['Franklin', 'Michel', 'Trevor'];
+console.log(friends);
+
+const year = new Array(1997, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Bob';
+console.log(friends);
+// friends = ['Peter', 'Alice'];
+
+const firstName = 'Sourav';
+const sourav = [firstName, 'Das', 2037 - 1997, 'learner', friends];
+console.log(sourav);
+console.log(sourav.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1997, 1967, 2002, 2010, 2020];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
 
 /*
 ////////////////////////////////////////////////////////////////////
