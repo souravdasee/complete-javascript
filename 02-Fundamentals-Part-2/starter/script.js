5c5b3245e8c847a08155b90d8de913591bcd6f1e
Sourav Das <souravdas4816@gmail.com>
2023-08-25 11:33:13 +0530

    The while Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+let rep = 1;
+while (rep <= 5) {
+    console.log(`WHILE: Lifting weights repetition ${rep} 🏋️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
+}
+
+/*
+///////////////////////////////////////////////////////////////////////
 const sourav = [
     'Sourav',
     'Das',
