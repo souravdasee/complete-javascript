d58d0b00493a5ed73d9b8f4b8c0b25649a940daf
Sourav Das <souravdas4816@gmail.com>
2023-08-25 12:22:05 +0530

    Coding Challenge #4

--- 02-Fundamentals-Part-2/starter/codingchallenge.js
+++ 02-Fundamentals-Part-2/starter/codingchallenge.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const calcTip = bills => bills >= 50 && bills<= 300 ? bills * 0.15 : bills * 0.2;
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+}
+
+console.log (bills, tips, totals);
+
+// Bonus: 
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0;  i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum/ arr.length;
+}
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+
+
+/*
+//////////////////////////////////////////////////////////////////////////
 const BMI = {
     name: ['Mark Miller', 'John Smith'],
     weight: [78, 92],
