d7c8b4a3082ecc7b53691a5f4d56eff6062fd9f3
Sourav Das <souravdas4816@gmail.com>
2023-08-25 09:23:57 +0530

    Coding Challenge #3

--- 02-Fundamentals-Part-2/starter/codingchallenge.js
+++ 02-Fundamentals-Part-2/starter/codingchallenge.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const BMI = {
+    name: ['Mark Miller', 'John Smith'],
+    weight: [78, 92],
+    height: [1.69, 1.95],
+    calcBMI: function() {
+        this.bmi1 = this.weight[0] / this.height[0] ** 2;
+        this.bmi2 = this.weight[1] / this.height[1] ** 2;
+        return [this.bmi1, this.bmi2];
+    }
+}
+
+console.log(BMI.calcBMI());
+
+if (BMI.calcBMI()[0] > BMI.calcBMI()[1]) {
+    console.log(`${BMI.name[0]}'s BMI (${BMI.calcBMI()[0]}) is higher than ${BMI.name[1]}'s (${BMI.calcBMI()[1]})!`);
+} else {
+    console.log(`${BMI.name[1]}'s BMI (${BMI.calcBMI()[1]}) is higher than ${BMI.name[0]}'s (${BMI.calcBMI()[0]})!`);
+}
+
+/*
+//////////////////////////////////////////////////////////////////////////
 const bills = [125, 555, 44];
 const calcTip = bills => bills >= 50 && bills<= 300 ? bills * 0.15 : bills * 0.2;
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
@@ -11,7 +32,7 @@ const total = [
 
 console.log(bills, tips, total);
 
-/*
+
 //////////////////////////////////////////////////////////////////////////
 const calcAverage = (s1, s2, s3) => (s1 + s2 + s3) / 3;
 let scoreDolphins = calcAverage(85, 54, 41);
