e84a1ffa4fa5cb922243c991b6974009f8210bc3
Sourav Das <souravdas4816@gmail.com>
2023-09-06 20:45:21 +0530

    Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,51 @@
 'use strict';
 
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Sourav';
+let job = 'teacher';
+const year = 1997;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example 1
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// Example 2
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+/*
+/////////////////////////////////////////////////////////////////////////
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -36,3 +82,4 @@ const firstName = 'Sourav';
 calcAge(1997);
 // console.log(age);
 // printAge();
+*/
