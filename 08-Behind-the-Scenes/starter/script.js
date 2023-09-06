8a54fbf3c036aaea88aa4e75313d13a33f3ecf2a
Sourav Das <souravdas4816@gmail.com>
2023-09-06 21:53:19 +0530

    Regular Functions vs. Arrow Functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,54 @@
 'use strict';
 
+// var firstName = 'Bob';
+
+const sourav = {
+  firstName: 'Sourav',
+  year: 1994,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   //   console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+sourav.greet();
+sourav.calcAge();
+
+// argument keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 4, 5, 66);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 3, 5);
+
+/*
+/////////////////////////////////////////////////////////////////////////
 console.log(this);
 
 const calcAge = function (birthYear) {
