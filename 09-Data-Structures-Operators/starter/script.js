77a9f87fdfb7ec468e33fd8ff2682d19921e2b66
Sourav Das <souravdas4816@gmail.com>
2023-09-14 22:36:11 +0530

    Working With Strings - Part 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -60,6 +60,57 @@ const restaurant = {
   },
 };
 
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Sourav Das'.split(' '));
+
+const [firstName, lastName] = 'Sourav Das'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('sourav das');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Sourav'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(43891134));
+console.log(maskCreditCard(323817318471));
+console.log(maskCreditCard(120371710312312));
+
+// Repeat
+const message2 = 'Bad weather... All Departures delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+///////////////////////////////////////////////////////////////////////////////
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
