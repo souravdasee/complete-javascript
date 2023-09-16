95b1a7a861d0e302abd609c5161602e74618c8b7
Sourav Das <souravdas4816@gmail.com>
2023-09-16 15:33:13 +0530

    How Passing Arguments Works Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,41 @@
 'use strict';
 
+const flight = 'LH234';
+const sourav = {
+  name: 'Sourav Das',
+  passport: 192371982373,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 192371982373) {
+    alert('Checked in!');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, sourav);
+console.log(flight);
+console.log(sourav);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = sourav;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+};
+
+newPassport(sourav);
+checkIn(flight, sourav);
+console.log(flight);
+console.log(sourav);
+
+/*
+////////////////////////////////////////////////////////////////////////////////////
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +61,4 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+*/
