7e5b8558468285b9d8d6b16d08773c5b0b9cfa16
Sourav Das <souravdas4816@gmail.com>
2023-09-17 09:53:44 +0530

    Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Sourav', 'Bob', 'Alice'].forEach(high5);
+
+/*
+////////////////////////////////////////////////////////////////////////////////////
 const flight = 'LH234';
 const sourav = {
   name: 'Sourav Das',
