6de335e3f0f1999cf198a8c2c284ba2c0bbe26e9
Sourav Das <souravdas4816@gmail.com>
2023-09-23 11:18:30 +0530

    Looping Arrays forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,6 +75,30 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('--- FOREACH ---');
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+// 0: function(200)
+// 0: function(450)
+// 0: function(-400)
+// ...
+
+/*
+/////////////////////////////////////////////////
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
