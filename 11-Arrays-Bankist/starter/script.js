39284cb1351f249b0640f3375c1ce2bb4f18a92a
Sourav Das <souravdas4816@gmail.com>
2023-09-28 11:16:32 +0530

    More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -235,6 +235,43 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+console.log(x.map(() => 5));
+
+x.fill(1, 3, 5);
+console.log(x);
+
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementUI);
+
+  const movementUI2 = [...document.querySelectorAll('.movements__value')];
+  // console.log(movementUI2);
+});
+
+/*
+/////////////////////////////////////////////////
 // Strings
 const owners = ['Sourav', 'Zack', 'Adam', 'Mark'];
 console.log(owners.sort());
