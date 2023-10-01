2a70c30da8da93beecf6c222c363e152844cd26d
Sourav Das <souravdas4816@gmail.com>
2023-10-01 18:50:54 +0530

    Creating Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -252,6 +252,42 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+////////////////////////008 Creating Dates/////////////////////////////
+
+const now = new Date();
+console.log(now); //->"Sun Oct 01 2023 18:10:39 GMT+0530 (India Standard Time)"
+
+console.log(new Date('Oct 01 2023 18:15:28')); //->"Sun Oct 01 2023 18:15:28 GMT+0530 (India Standard Time)"
+console.log(new Date('December 24, 2023')); //->"Sun Dec 24 2023 00:00:00 GMT+0530 (India Standard Time)"
+console.log(new Date(account1.movementsDates[0])); //->"Tue Nov 19 2019 03:01:17 GMT+0530 (India Standard Time)"
+
+console.log(new Date(2037, 10, 19, 15, 23, 5)); //->"Thu Nov 19 2037 15:23:05 GMT+0530 (India Standard Time)"
+console.log(new Date(2037, 10, 31)); //->"Tue Dec 01 2037 00:00:00 GMT+0530 (India Standard Time)"
+
+console.log(new Date(0)); //->"Thu Jan 01 1970 05:30:00 GMT+0530 (India Standard Time)"
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); //->"Sun Jan 04 1970 05:30:00 GMT+0530 (India Standard Time)"
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future); //->"Thu Nov 19 2037 15:23:00 GMT+0530 (India Standard Time)"
+console.log(future.getFullYear()); //->2037
+console.log(future.getMonth()); //->10
+console.log(future.getDate()); //->19
+console.log(future.getDay()); //->4
+console.log(future.getHours()); //->15
+console.log(future.getMinutes()); //->23
+console.log(future.getSeconds()); //->0
+console.log(future.getTime()); //->2142237180000
+console.log(future.toISOString()); //->"2037-11-19T09:53:00.000Z"
+
+console.log(new Date(2142237180000)); //->"Thu Nov 19 2037 15:23:00 GMT+0530 (India Standard Time)"
+
+console.log(Date.now()); //->1696165199453
+
+future.setFullYear(2040);
+console.log(future);  //->"Mon Nov 19 2040 15:23:00 GMT+0530 (India Standard Time)"
+
+/*
 //////////////////////007 Working with BigInt//////////////////////////
 
 console.log(2 ** 53 - 1); //->9007199254740991
