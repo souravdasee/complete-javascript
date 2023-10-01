0f82f8c93a87a1137fa0cb93481d9ee99210fe72
Sourav Das <souravdas4816@gmail.com>
2023-10-01 17:09:58 +0530

    Numeric Separators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -252,6 +252,24 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+//////////////////////006 Numeric Separators///////////////////////////
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter); //->287460000000
+
+const price = 345_99;
+console.log(price); //->34599
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI); //->3.1415
+
+console.log(Number('230_000')); //->NaN
+console.log(parseInt('230_000')); //->230
+
+/*
 /////////////////////005 The Remainder Operator////////////////////////
 console.log(5 % 2); //->1
 console.log(5 / 2); // 5 = 2 * 2 + 1  ->2.5
