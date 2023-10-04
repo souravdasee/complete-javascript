895517d35ae56072acf81f65cebaa882624f7b25
Sourav Das <souravdas4816@gmail.com>
2023-10-05 01:23:46 +0530

    Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,39 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X|Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'Height|Width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/*
 /////////005 Selecting, Creating, and Deleting Elements////////////////////////////////
 
 // Selecting elements
@@ -118,3 +151,4 @@ logo.classList.contains('c'); // not includes
 
 // Do not use this way because it overrides all classes in that element to setted one
 // logo.className = 'Sourav';
+*/
