872d4e78c00a7cee45b2f3d328306180e78e3021
Sourav Das <souravdas4816@gmail.com>
2023-10-04 21:48:19 +0530

    Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,7 +30,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-/////////////////
+/////////005 Selecting, Creating, and Deleting Elements////////////////////////////////
 
 // Selecting elements
 console.log(document.documentElement);
@@ -70,3 +70,51 @@ document
     message.remove();
     // message.parentElement.removeChild(message);  // Old way of deleting elemnts
   });
+
+/////////006 Styles, Attributes and Classes//////////////////////
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '104%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Do not use this way because it overrides all classes in that element to setted one
+// logo.className = 'Sourav';
