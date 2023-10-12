c2421026f6df9ece52a542488def866874c9bc05
Sourav Das <souravdas4816@gmail.com>
2023-10-12 16:43:59 +0530

    (Fixed) Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,46 +1,8 @@
 'use strict';
 
 ///////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
-
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
-
-GOOD LUCK 😀
-*/
-
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
-
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.accelerate();
-
-/*
-///////////////////////////////////////
 // Constructor Functions and the new Operator
+
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -62,62 +24,9 @@ console.log(sourav);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+const jay = 'Jay';
 
 console.log(sourav instanceof Person);
-
-Person.hey = function () {
-  console.log('Hey there 👋');
-  console.log(this);
-};
-Person.hey();
-
-///////////////////////////////////////
-// Prototypes
-console.log(Person.prototype);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
-
-sourav.calcAge();
-matilda.calcAge();
-
-console.log(sourav.__proto__);
-console.log(sourav.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(sourav));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-// .prototyeOfLinkedObjects
-
-Person.prototype.species = 'Homo Sapiens';
-console.log(sourav.species, matilda.species);
-
-console.log(sourav.hasOwnProperty('firstName'));
-console.log(sourav.hasOwnProperty('species'));
-
-///////////////////////////////////////
-// Prototypal Inheritance on Built-In Objects
-console.log(sourav.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(sourav.__proto__.__proto__);
-console.log(sourav.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
-*/
+console.log(jay instanceof Person);
