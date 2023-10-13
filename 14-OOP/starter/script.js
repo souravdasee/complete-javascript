463f706bc36bc9d1b0399b3f14509156097825ff
Sourav Das <souravdas4816@gmail.com>
2023-10-13 10:38:53 +0530

    Another Class Example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -468,6 +468,7 @@ martha.introduce();
 martha.calcAge();
 */
 
+/*
 ///////////////////////////////////////
 // Inheritance Between "Classes": Object.create
 
@@ -499,3 +500,51 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+/////////////////////////////////////////////
+// Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface or API
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Sourav', 'INR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(100);
+
+console.log(acc1);
+console.log(acc1.pin);
