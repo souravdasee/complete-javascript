6e396cbb3ef95d1960e89d0a9db18a278fc214f9
Sourav Das <souravdas4816@gmail.com>
2023-11-06 13:59:04 +0530

    Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -31,8 +31,37 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`Country not found (${response.status})`);
+
+    return response.json();
+  });
+};
+
 ///////////////////////////////////////
 
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log([data1.capital[0], data2.capital[0], data3.capital[0]]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital[0]));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('spain', 'portugal', 'germany');
+
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -93,6 +122,7 @@ console.log('1: Will get location');
   }
   console.log('3: Finished getting location');
 })();
+*/
 
 /*
 ///////////////////////////////////////
@@ -283,14 +313,6 @@ console.log('Test end');
 */
 
 /*
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`Country not found (${response.status})`);
-
-    return response.json();
-  });
-};
-
 const getCountryData = function (country) {
   // Country 1
   getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
