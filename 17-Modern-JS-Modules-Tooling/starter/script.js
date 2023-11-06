2d123513d8e8a3155afe4c0969e54c49b6cd2b0a
Sourav Das <souravdas4816@gmail.com>
2023-11-06 17:52:19 +0530

    Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -20,3 +20,25 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lasPost = getLastPost();
+console.log(lasPost);
+
+// Not very clean
+// lasPost.then(last => console.log(last));
+
+const lasPost2 = await getLastPost();
+console.log(lasPost2);
